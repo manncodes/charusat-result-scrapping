6931ce37dbae4636975620ea146f2fccdbb4c9d2
Madhav Tripathi <madhav.37.tripathi@gmail.com>
2021-02-05 12:05:55 +0530

    added d2d ids removed unused imports

--- scrape.py
+++ scrape.py
@@ -1,18 +1,14 @@
 import math
-import requests
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from threading import Thread, Barrier
 from selenium import webdriver
 import pandas as pd
 
 
 driver = webdriver.Chrome()
-resultlist = []    
+resultlist = []
 
-for i in range(1,136):
+for i in range(1, 165):
     try:
         driver.get("http://117.239.83.200:2020/frmUniversityResult.aspx")
         e1 = Select(driver.find_element_by_id("ddlInst"))
@@ -23,18 +19,18 @@ for i in range(1,136):
         e3.select_by_value('5')
         e4 = Select(driver.find_element_by_id("ddlScheduleExam"))
         e4.select_by_value('4917')
-        ID = '18DCS'
+        ID = 'D19DCS' if i > 135 else '18DCS'
         for j in range(2-int(math.log10(i))):
-            ID=ID + '0'
-        ID=ID + str(i)
+            ID = ID + '0'
+        ID = ID + str(i)
         element = driver.find_element_by_id("txtEnrNo")
         element.send_keys(ID)
         try:
             driver.find_element_by_id("btnSearch").click()
             doc = BeautifulSoup(driver.page_source, "html.parser")
-            Name = doc.find('span',id='uclGrd1_lblStudentName')
-            SGPA = doc.find('span',id='uclGrd1_lblSGPA')
-            result={}
+            Name = doc.find('span', id='uclGrd1_lblStudentName')
+            SGPA = doc.find('span', id='uclGrd1_lblSGPA')
+            result = {}
             result['ID'] = ID
             result['name'] = Name.text
             result['SGPA'] = SGPA.text
@@ -44,15 +40,7 @@ for i in range(1,136):
             pass
     except:
         pass
-    
+
 
 df = pd.DataFrame(resultlist)
-df.to_csv('results.csv',index = False)
-
-
-
-        
-
-
-
-
+df.to_csv('results.csv', index=False)
